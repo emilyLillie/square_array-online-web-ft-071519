d94109d353eda57e2fb710dedc4f423767c280d5
Emily Lillie <emilylillie3@gmail.com>
2019-07-17 14:05:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,8 +6,4 @@ def square_array(numbers)
   return new_numbers
 end
 
-def square_array(numbers)
-  old_numbers = []
-  squared_numbers = old_numbers.each do |number| number **2 end
-  return squared_numbers
-end
+
